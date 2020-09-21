642304c9fcd65ac2c379f389789817731a5408c9
JohnsonNYC <jkow95@gmail.com>
2020-09-21 17:00:43 -0400

    Dropped and reseeded the database

--- iheart-backend/app.py
+++ iheart-backend/app.py
@@ -2,11 +2,13 @@ from flask import Flask, request, jsonify
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 import os
 
 # Init app
 
 app = Flask(__name__)
+CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
@@ -46,6 +48,67 @@ class SongSchema(ma.Schema):
 # Init Schema
 song_schema = SongSchema()
 songs_schema = SongSchema(many=True)
+
+# Db creation flask commands
+
+
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database Created!')
+
+
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('Database Dropped!')
+
+
+@app.cli.command('db_seed')
+def db_seed():
+    song1 = Song(title='Pretty Boy', artist='The Neighbourhood',
+                 genre='Alternative', year=2020)
+    song2 = Song(title='Tui', artist='FKJ', genre='Electronic', year=2018)
+    song3 = Song(title='Love Bomb', artist='East Forrest',
+                 genre='Electronic', year=2012)
+    song4 = Song(title='Years', artist='DRAMA', genre='Electronic', year=2020)
+    song5 = Song(title='Cinnamon', artist='Jome',
+                 genre='Alternative', year=2016)
+    song6 = Song(title='Liminal', artist='The Acid',
+                 genre='Alternative', year=2016)
+    song7 = Song(title='ii', artist='Aquilo', genre='Alternative', year=2017)
+    song8 = Song(title='Pacific II', artist='Goth Babe',
+                 genre='Alternative', year=2018)
+    song9 = Song(title='Summers Gone', artist='Nombe',
+                 genre='Alternative', year=2018)
+    song10 = Song(title='Its okay, youre okay', artist='Bonjr',
+                  genre='Alternative', year=2018)
+    song11 = Song(title='Distance', artist='Hippie Sabotage',
+                  genre='Electronic', year=2018)
+    song12 = Song(title='Notion', artist='Tash Saltuna',
+                  genre='Alternative', year=2016)
+    song13 = Song(title='Mutual Love', artist='Shallou',
+                  genre='Electronic', year=2020)
+    song14 = Song(title='Dance', artist='Aaron Smith',
+                  genre='Dance', year=2014)
+
+    db.session.add(song1)
+    db.session.add(song2)
+    db.session.add(song3)
+    db.session.add(song4)
+    db.session.add(song5)
+    db.session.add(song6)
+    db.session.add(song7)
+    db.session.add(song8)
+    db.session.add(song9)
+    db.session.add(song10)
+    db.session.add(song11)
+    db.session.add(song12)
+    db.session.add(song13)
+    db.session.add(song14)
+
+    db.session.commit()
+    print('Database Seeded!')
 
 # Create Song
 
